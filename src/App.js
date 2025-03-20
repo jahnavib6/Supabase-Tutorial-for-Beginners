b7a0772c542bb9e6757b36415b9f02015cafe3ad
jahnavib6 <bulusujahnavi06@gmail.com>
2025-03-19 20:11:59 -0400

    more recommendation stuff (router and links)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import Create from "./pages/Create"
 import Update from "./pages/Update"
+import Recs from ".pages/Recs"
+import CreateRecs from ".pages/CreateRecs"
 
 
 function App() {
@@ -13,11 +15,15 @@ function App() {
         <h1>Supa Smoothies</h1>
         <Link to="/">Home</Link>
         <Link to="/create">Create New Smoothie</Link>
+        <Link to="/">Recommendations</Link>
+        <Link to="/create">Give Recommendation</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
         <Route path="/:id" element={<Update />} />
+        <Route path="/recommendations" element={<Recs />} />
+        <Route path="/create-recommendation" element={<CreateRecs />} />
       </Routes>
     </BrowserRouter>
   );
